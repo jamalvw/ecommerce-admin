6759ac8855bd0c7c4711f216d1f0c28e815ef401
jamalvw <jvwill00@gmail.com>
2023-08-31 20:00:02 -0400

    Add STORE_GET to API

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,6 +2,28 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+export async function GET (
+    _: Request,
+    { params }: { params: { storeId: string } }
+) {
+    try {
+        if (!params.storeId)
+            return new NextResponse('Store ID is required', { status: 400 });
+            
+        const store = await prismadb.store.findUnique({
+            where: {
+                id: params.storeId,
+            }
+        });
+
+        return NextResponse.json(store);
+    } catch (error) {
+        console.log('[STORE_GET]', error);
+        return new NextResponse('Internal error', { status: 500 });
+    }
+}
+
+
 export async function PATCH (
     req: Request,
     { params }: { params: { storeId: string } }
